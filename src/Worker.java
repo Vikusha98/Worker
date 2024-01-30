8e4c8c126a75f61e85059c551938574165bec9d7
Vikusha <victoriaflint01@gmail.com>
2024-01-30 18:28:44 +0300

    Initial commit

---
+++ src/Worker.java
@@ -0,0 +1,31 @@
+public class Worker {
+    private OnTaskDoneListener callback;
+    private OnTaskErrorListener errorCallback;
+
+    public Worker(OnTaskDoneListener callback, OnTaskErrorListener errorCallback) {
+        this.callback = callback;
+        this.errorCallback = errorCallback;
+    }
+
+    public void start() {
+        for (int i = 0; i < 100; i++) {
+            if (i == 33) {
+                errorCallback.onError("Ошибка возникает в задаче " + i);
+            } else {
+                callback.onDone("Задача " + i + " выполнена");
+            }
+        }
+    }
+
+    @FunctionalInterface
+    public interface OnTaskDoneListener {
+        void onDone(String result);
+    }
+
+    @FunctionalInterface
+    public interface OnTaskErrorListener {
+        void onError(String errorMessage);
+    }
+}
+
+
